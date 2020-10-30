7fe7501c5848323ab35e295008ce4d636150debd
Adam Denes <adamdenes@icloud.com>
2020-10-30 12:41:55 +0100

    adding new links

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from datetime import time
-from flask import (Flask, g, render_template, flash, redirect, url_for)
+from flask import (Flask, g, render_template, flash,
+                   redirect, url_for, request)
 from flask_bcrypt import generate_password_hash
 
 import os
@@ -16,6 +16,7 @@ SECRET = os.urandom(24).hex()
 
 app = Flask(__name__)
 app.secret_key = generate_password_hash(SECRET)
+
 
 @app.before_request
 def before_request():
@@ -46,16 +47,17 @@ def entries():
 def add():
     """Adding journal entry to the database and view."""
     form = forms.AddEntryForm()
+
     if form.validate_on_submit():
-        models.Journal.create_journal(
+        new_entry = models.Journal.create_journal(
             title=form.journal_title.data,
             date=form.journal_date.data,
             time_spent=form.journal_time_spent.data,
             learned=form.journal_learned.data,
             resources=form.journal_resources.data
         )
-        flash('New journal entry has been created!', 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('detail', new_entry=new_entry))
+        
     return render_template('new.html', form=form)
 
 
@@ -65,7 +67,7 @@ def detail(id):
     try:
         entry = models.Journal.select().where(
             models.Journal.id == id
-            ).get()
+        ).get()
     except models.DoesNotExist:
         return redirect(url_for('index'))
     return render_template('detail.html', entry=entry)
@@ -75,6 +77,7 @@ def detail(id):
 def edit():
     pass
 
+
 @app.route('/entries/<id>/delete')
 def delete():
     pass
@@ -82,7 +85,7 @@ def delete():
 
 if __name__ == '__main__':
     models.initialize()
-    
+
     # creating a test entry
     try:
         models.Journal.create_journal(
