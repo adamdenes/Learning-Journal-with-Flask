25737e74becf7e4bc32d68473f7ac21e8997d4f5
Adam Denes <adamdenes@icloud.com>
2020-10-30 11:55:23 +0100

    new entry

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from datetime import time
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask_bcrypt import generate_password_hash
 
 import os
+
+from peewee import IntegrityError
 import models
+import forms
 
 
 DEBUG = True
@@ -38,14 +42,26 @@ def entries():
     return redirect(url_for('index'))
 
 
-@app.route('/entries/new')
-def new():
-    pass
+@app.route('/entries/new', methods=('GET', 'POST'))
+def add():
+    """Adding journal entry to the database and view."""
+    form = forms.AddEntryForm()
+    if form.validate_on_submit():
+        models.Journal.create_journal(
+            title=form.journal_title.data,
+            date=form.journal_date.data,
+            time_spent=form.journal_time_spent.data,
+            learned=form.journal_learned.data,
+            resources=form.journal_resources.data
+        )
+        flash('New journal entry has been created!', 'success')
+        return redirect(url_for('index'))
+    return render_template('new.html', form=form)
 
 
 @app.route('/entries/<int:id>')
 def detail(id):
-    """Detail route/view."""
+    """Detail route/view. Displays every information about the given entry."""
     try:
         entry = models.Journal.select().where(
             models.Journal.id == id
@@ -66,6 +82,8 @@ def delete():
 
 if __name__ == '__main__':
     models.initialize()
+    
+    # creating a test entry
     try:
         models.Journal.create_journal(
             title='Test title',
@@ -75,4 +93,5 @@ if __name__ == '__main__':
         )
     except ValueError:
         pass
+
     app.run(debug=DEBUG, port=PORT, host=HOST)
